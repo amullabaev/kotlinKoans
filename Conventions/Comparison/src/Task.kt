b81fab0568a8e5762fd189f384bbbc6ab93e1f2c
amullabaev <a.mullabaev@gmail.com>
2019-08-19 20:57:32 +0600

    Comparison task solution

--- Conventions/Comparison/src/Task.kt
+++ Conventions/Comparison/src/Task.kt
@@ -1,5 +1,13 @@
+import java.util.*
+
 data class MyDate(val year: Int, val month: Int, val dayOfMonth: Int) : Comparable<MyDate> {
-    /* TODO */
+    override fun compareTo(other: MyDate): Int {
+        val firstDate = Calendar.getInstance()
+        firstDate.set(year, month, dayOfMonth)
+        val secondDate = Calendar.getInstance()
+        secondDate.set(other.year, other.month, other.dayOfMonth)
+        return firstDate.compareTo(secondDate)
+    }
 }
 
 fun compare(date1: MyDate, date2: MyDate) = date1 < date2
