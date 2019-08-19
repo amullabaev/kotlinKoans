fd83492f208c6b12fb3e056e2086a07258778476
amullabaev <a.mullabaev@gmail.com>
2019-08-19 20:56:23 +0600

    changed solution for Object expression and SAM conversions tasks

--- Introduction/Object expressions/src/Task.kt
+++ Introduction/Object expressions/src/Task.kt
@@ -2,14 +2,7 @@ import java.util.*
 
 fun getList(): List<Int> {
     val arrayList = arrayListOf(1, 5, 2)
-    arrayList.sortWith(Comparator { o1, o2 ->
-        if (o1 > o2) {
-            -1
-        } else if (o1 < o2) {
-            1
-        } else {
-            0
-        }
-    })
+    Collections.sort(arrayList, { o1, o2 -> o2 - o1 })
+
     return arrayList
 }
