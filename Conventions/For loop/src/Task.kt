89e874585fa054ab6095d0b195f4cd3eb49f17d0
amullabaev <a.mullabaev@gmail.com>
2019-08-20 21:04:07 +0600

    solved For Loop problem

--- Conventions/For loop/src/Task.kt
+++ Conventions/For loop/src/Task.kt
@@ -1,4 +1,23 @@
-class DateRange(val start: MyDate, val end: MyDate)
+class DateRange(val start: MyDate, val end: MyDate): Iterable<MyDate> {
+    override fun iterator(): Iterator<MyDate> {
+        return MyDateIterator(this)
+    }
+}
+
+class MyDateIterator(private val dateRange: DateRange): Iterator<MyDate> {
+
+    var current: MyDate = dateRange.start
+
+    override fun hasNext(): Boolean {
+        return current <= dateRange.end
+    }
+
+    override fun next(): MyDate {
+        val data = current
+        current = current.nextDay()
+        return data
+    }
+}
 
 fun iterateOverDateRange(firstDate: MyDate, secondDate: MyDate, handler: (MyDate) -> Unit) {
     for (date in firstDate..secondDate) {
