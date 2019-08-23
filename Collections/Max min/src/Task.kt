a7c79dd24e2862ab38940c686f76a7208c140d51
amullabaev <a.mullabaev@gmail.com>
2019-08-23 18:00:52 +0600

    solved some tasks from Collections part

--- Collections/Max min/src/Task.kt
+++ Collections/Max min/src/Task.kt
@@ -1,5 +1,8 @@
 // Return a customer whose order count is the highest among all customers
-fun Shop.getCustomerWithMaximumNumberOfOrders(): Customer? = TODO()
+fun Shop.getCustomerWithMaximumNumberOfOrders(): Customer? = customers.maxBy { it.orders.size }
 
 // Return the most expensive product which has been ordered
-fun Customer.getMostExpensiveOrderedProduct(): Product? = TODO()
+fun Customer.getMostExpensiveOrderedProduct(): Product? {
+    val products = orders.flatMap { it.products }
+    return products.maxBy { it.price }
+}
